b829b563ab65711d1b3380ac6c3e583d1221adca
tamss3 <63630722+tamss3@users.noreply.github.com>
2021-01-14 01:08:22 +0700

    Update p5js-temp-TAM3172487449166486242.js

--- TAM/p5js-temp-TAM3172487449166486242.js
+++ TAM/p5js-temp-TAM3172487449166486242.js
@@ -11,7 +11,7 @@ function setup() {
 
   // textToPoints has 'text', x location in canvas, y location, and font size
   // creates all of the points from the text passcode
-  var points = font.textToPoints('DENINN. NDA',10,400, 70);
+  var points = font.textToPoints('DELA.P.K',10,400, 70);
 
   //  for each point in the text, create a random start position and a vector to where it needs to end up
   for (var i=0; i < points.length; i++){
